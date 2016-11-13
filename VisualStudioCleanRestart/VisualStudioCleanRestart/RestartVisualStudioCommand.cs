a44072fc5dc2fa849505dedc6acd99980edcc1d3
Marc Lammers <lammers.marc@gmail.com>
2016-11-13 21:26:13 +0100

    Init Project.

---
+++ VisualStudioCleanRestart/VisualStudioCleanRestart/RestartVisualStudioCommand.cs
@@ -0,0 +1,148 @@
+﻿#region Imports
+
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.IO;
+
+using EnvDTE80;
+
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+#endregion
+
+namespace VisualStudioCleanRestart
+{
+    /// <summary>
+    ///     Command handler
+    /// </summary>
+    internal sealed class RestartVisualStudioCommand
+    {
+        #region Constants
+
+        /// <summary>
+        ///     Command ID.
+        /// </summary>
+        public const int COMMAND_ID = 0x0100;
+
+        #endregion
+
+        #region Fields
+
+        /// <summary>
+        ///     Command menu group (command set GUID).
+        /// </summary>
+        private static readonly Guid _commandSet = new Guid("0b5342e7-d6f2-45cf-8ed2-821097006467");
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="RestartVisualStudioCommand" /> class.
+        ///     Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private RestartVisualStudioCommand(Package package)
+        {
+            if (package == null)
+                throw new ArgumentNullException(nameof(package));
+
+            this.Package = package;
+
+            var commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(_commandSet, COMMAND_ID);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///     Gets the instance of the command.
+        /// </summary>
+        public static RestartVisualStudioCommand Instance { get; private set; }
+
+        /// <summary>
+        ///     VS Package that provides this command, not null.
+        /// </summary>
+        public Package Package { get; }
+
+        /// <summary>
+        ///     Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider => this.Package;
+
+        #endregion
+
+        #region Method
+
+        /// <summary>
+        ///     Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new RestartVisualStudioCommand(package);
+        }
+
+        #endregion
+
+        #region Subroutines
+
+        private ProcessStartInfo GetScriptProcess(string solutionPath, string vsPath)
+        {
+            var file = Path.GetTempFileName() + ".ps1";
+            using (var sw = new StreamWriter(file))
+            {
+                //sw.WriteLine("Write - Host \"Finished. Press any key to continue ...\"$x = $host.UI.RawUI.ReadKey(\"NoEcho,IncludeKeyDown\")");
+                sw.WriteLine("Start-Sleep -Seconds 5");
+                sw.WriteLine($"cd \"{Path.GetDirectoryName(solutionPath)}\"");
+                sw.WriteLine("Write-Host \"Current path:\"");
+                sw.WriteLine("Convert-Path .");
+                sw.WriteLine("Start-Sleep -Seconds 5");
+                sw.WriteLine("Write-Host \"Cleaning all bin and obj folders recursive...\"");
+                sw.WriteLine("Get-ChildItem -inc bin,obj -rec | Remove-Item -rec -force");
+                sw.WriteLine("Write-Host \"Restart Visual Studio...\"");
+                sw.WriteLine($"& \"{vsPath}\" \"{solutionPath}\"");
+            }
+
+            return new ProcessStartInfo
+            {
+                FileName = @"powershell.exe",
+                ErrorDialog = true,
+                UseShellExecute = true,
+                Arguments = $" –ExecutionPolicy Unrestricted -noexit {Path.GetFullPath(file)}"
+            };
+        }
+
+        /// <summary>
+        ///     This function is the callback used to execute the command when the menu item is clicked.
+        ///     See the constructor to see how the menu item is associated with this function using
+        ///     OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            var dte2 = Package.GetGlobalService(typeof(SDTE)) as DTE2;
+            if (dte2 == null)
+                return;
+            var currentProcess = Process.GetCurrentProcess();
+            var vsFile = currentProcess.MainModule.FileName;
+            var fullName = dte2.Solution.FullName;
+
+            var process = this.GetScriptProcess(fullName, vsFile);
+            Process.Start(process);
+            dte2.Quit();
+        }
+
+        #endregion
+    }
+}
