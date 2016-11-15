aacd9aa853b245eb413bd044d2b7ff31bc12d915
Marc Lammers <lammers.marc@gmail.com>
2016-11-15 22:52:56 +0100

    - Properties added to ViewModel - Controls added to View

--- VisualStudioCleanRestart/CleanStart/ViewModels/ProgressWindowViewModel.cs
+++ VisualStudioCleanRestart/CleanStart/ViewModels/ProgressWindowViewModel.cs
@@ -3,10 +3,48 @@
 namespace CleanStart.ViewModels
 {
     /// <summary>
-    /// Class ProgessWindowViewModel.
+    ///     Class ProgessWindowViewModel.
     /// </summary>
     public class ProgressWindowViewModel : BindableBase
     {
+        #region Fields
 
+        private string statusMessage;
+        private double progress;
+
+        #endregion
+
+        #region Constructor
+
+        public ProgressWindowViewModel()
+        {
+            this.StatusMessage = "Please wait...";
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///     Gets or sets the progress.
+        /// </summary>
+        /// <value>The progress.</value>
+        public double Progress
+        {
+            get { return this.progress; }
+            set { this.SetProperty(ref this.progress, value); }
+        }
+
+        /// <summary>
+        ///     Gets or sets the status message.
+        /// </summary>
+        /// <value>The status message.</value>
+        public string StatusMessage
+        {
+            get { return this.statusMessage; }
+            set { this.SetProperty(ref this.statusMessage, value); }
+        }
+
+        #endregion
     }
 }
